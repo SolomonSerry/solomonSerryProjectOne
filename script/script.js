35940d87efbf74441be15750ae122ab40fe1c4e3
Solomon <solo.serry@gmail.com>
2021-12-13 22:18:53 -0500

    Added slide-out menu with javascript event listener 'click':ocean::ocean::ocean:

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Want to create a feature that has the menu bar toggle in and out as you click the hamburger menu.
+
+const hMenu = document.querySelector('i');
+
+hMenu.addEventListener('click', function(event){
+  const navBar = document.querySelector ('ul');
+  
+  navBar.classList.toggle('isActive')
+});
+
