4c9ca66c9046b1884394fd90c23d7fceeebc7afc
Solomon <solo.serry@gmail.com>
2021-12-14 21:46:47 -0500

    Made some new chages to the slide out nav menu :fire::fire::fire:

--- script/script.js
+++ script/script.js
@@ -5,6 +5,8 @@ const hMenu = document.querySelector('i');
 hMenu.addEventListener('click', function(event){
   const navBar = document.querySelector ('ul');
   
+
   navBar.classList.toggle('isActive')
+  hMenu.classList.toggle('fa-times')
 });
 
