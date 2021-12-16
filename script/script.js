d70b7dfc8afa4d13b9b59d6b7d404493c775b4e6
Solomon <solo.serry@gmail.com>
2021-12-15 22:32:55 -0500

    Added name spacing to my java script :rocket::rocket::rocket:

--- script/script.js
+++ script/script.js
@@ -1,12 +1,23 @@
-// Want to create a feature that has the menu bar toggle in and out as you click the hamburger menu.
+// Want to create a feature that has the menu bar toggle in and out from the right sided as you click the hamburger menu, also have the hamburger menu icon switch to and 'x' went the menu is out and switch back to the hamburger once the menu slides back in.
 
-const hMenu = document.querySelector('i');
+// namespacing
+const textileeApp = {};
 
-hMenu.addEventListener('click', function(event){
-  const navBar = document.querySelector ('ul');
+// init function
+textileeApp.init = function() {
+  textileeApp.slidingNavMenu();
+};
+
+// create nav sliding function
+textileeApp.slidingNavMenu = function() {
+  const hMenu = document.querySelector('i');
   
+   hMenu.addEventListener('click', function(event){
+    const navBar = document.querySelector ('ul');
+  
+      navBar.classList.toggle('isActive');
+      hMenu.classList.toggle('fa-times');
+  });
+};
 
-  navBar.classList.toggle('isActive')
-  hMenu.classList.toggle('fa-times')
-});
-
+textileeApp.init();
